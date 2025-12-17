2c1fbaecec5961322df85af4449bb75600dacc6b
Rap Ramos <reiallenramos@gmail.com>
2025-12-17 20:18:58 +1000

    Initial Commit

---
+++ MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by Rap Ramos on 17/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
